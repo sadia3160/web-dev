102f28325e1f60fd388f1157375e03d82d03954d
Sadia Sultana <sadias11.cse@gmail.com>
2026-07-16 13:51:26 +0600

    Update chapter1.js

--- part-i/chapter1.js
+++ part-i/chapter1.js
@@ -5,4 +5,4 @@ console.log("Metropolitan University");
 //Used for printing, debugging and testing to the developer's console
 console.log(5+2);
 
-console.log("bye);
+console.log("bye); //Syntax error
