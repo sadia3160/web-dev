e2b01888822e8942f289839e0c0068ef4d996b67
Sadia Sultana <sadias11.cse@gmail.com>
2026-07-16 13:49:28 +0600

    chapter 1 excercise done

---
+++ part-i/chapter1.js
@@ -0,0 +1,8 @@
+console.log("Sadia Sultana");
+console.log("Computer Science and Engineering");
+console.log("Metropolitan University");
+
+//Used for printing, debugging and testing to the developer's console
+console.log(5+2);
+
+console.log("bye);
